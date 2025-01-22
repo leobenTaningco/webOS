322ecc4ca75bfb7021e2ef2aa3657dd99326cf32
leobenTaningco <leotaningco@gmail.com>
2025-01-23 03:36:27 +0800

    spring cleaning + added time

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,37 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
 import "./home.css"; // Assuming home.css is in the same directory
 
 const Home = () => {
-  // State to handle whether the home menu is active or not
-  const [isMenuActive, setMenuActive] = useState(false);
+    const [isMenuActive, setMenuActive] = useState(false);
+    const [dateTime, setDateTime] = useState(new Date());
 
-  // Toggle menu visibility
-  const handleTaskbarClick = () => {
+    // Toggle menu visibility
+    const handleTaskbarClick = () => {
     setMenuActive(!isMenuActive);
-  };
+    };
 
-  return (
+    // Get date and time
+    useEffect(() => {
+        const timer = setInterval(() => {
+          setDateTime(new Date());
+        }, 1000); // Update every 1 second
+    
+        // When not rendered, it stops the clock, saving memory
+        return () => clearInterval(timer);
+    }, []);
+    
+    const formattedTime = `${dateTime.toLocaleTimeString()} `;
+
+    return (
     <div>
-      <div className="bg"></div>
-      <div className="taskbar">
-        <div
-          className="taskbar-start"
-          onClick={handleTaskbarClick}
-        >
-            <div className={`home-menu ${isMenuActive ? "active" : ""}`}>
-                {isMenuActive && ( // If true, it renders everything inside this parenthesis
-                    <div className="footer-menu">
-                    <Link to="/" className="logout"></Link>
-                    <div className="user-pic"></div>
-                    <div className="user-name">Yzowe</div>
-                    </div>
-                )}    
+        <div className="bg"></div>
+        <div className="taskbar">
+            <div
+            className="taskbar-start"
+            onClick={handleTaskbarClick}
+            >
+                <div className={`home-menu ${isMenuActive ? "active" : ""}`}>
+                    {isMenuActive && ( // If true, it renders everything inside this parenthesis
+                        <div className="footer-menu">
+                        <Link to="/" className="logout"></Link>
+                        <div className="user-pic"></div>
+                        <div className="user-name">Yzowe</div>
+                        </div>
+                    )}    
+                </div>
             </div>
+            <div
+            className="taskbar-time"
+            >
+                <div className="time-date">
+                    {formattedTime}
+                </div>
+            </div>
+
         </div>
-      </div>
     </div>
-  );
+    );
 };
 
 export default Home;
--- src/Login.jsx
+++ src/Login.jsx
@@ -2,18 +2,18 @@ import "./login.css";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
+    return (
     <div>
-      <div className="bg-blurred"></div>
+        <div className="bg-blurred"></div>
 
-      <div>
-        <Link to="/home">
-          <div className="profile-button">
-            <div className="profile-text">hi :)</div>
-            <div className="profile-name">Yzowe</div>
-          </div>
-        </Link>
-      </div>
+        <div>
+            <Link to="/home">
+            <div className="profile-button">
+                <div className="profile-text">hi :)</div>
+                <div className="profile-name">Yzowe</div>
+            </div>
+            </Link>
+        </div>
     </div>
   );
 };
