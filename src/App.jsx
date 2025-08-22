63c39bd4ad903bb3252df725afa933ea85d2ceda
leobenTaningco <152823492+leobenTaningco@users.noreply.github.com>
2025-08-22 05:49:37 +0000

    test: add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  
 import Login from './Login.jsx';  // 
 import Home from './Home.jsx';  // 
@@ -6,6 +7,7 @@ import Home from './Home.jsx';  //
 function App() {
   return (
     <Router> {/* This wraps the entire app to enable routing */}
+      <Analytics />
       <Routes> 
         <Route path="/" element={<Home />} /> 
         <Route path="/home" element={<Login />} /> 
