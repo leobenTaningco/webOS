1dab42398c108159f45ed860c299ec18a6d6c1be
leobenTaningco <152823492+leobenTaningco@users.noreply.github.com>
2025-08-22 06:09:29 +0000

    test: add speedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  
 import Login from './Login.jsx';  // 
 import Home from './Home.jsx';  // 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router> {/* This wraps the entire app to enable routing */}
       <Analytics />
+      <SpeedInsights />
       <Routes> 
         <Route path="/" element={<Home />} /> 
         <Route path="/home" element={<Login />} /> 
